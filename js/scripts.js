cdac869f66e92cf8add423c1b2f852d9ddbb130c
Alexandra Holcombe and Marc Larkin <marc@>
2017-01-24 09:40:50 -0800

    add javascript and html to create an address section

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,14 @@
 function Contact(first, last) {
 	this.firstName = first;
 	this.lastName = last;
+  this.addresses = []
 };
+
+function Address(street, city, state) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
+}
 
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
@@ -10,6 +17,22 @@ Contact.prototype.fullName = function() {
 
 //User Interface Logic
 $(document).ready(function(){
+  $("#add-address").click(function() {
+    $("#new-addresses").append('<div class="new-address">' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-street">Street</label>' +
+                                   '<input type="text" class="form-control new-street">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-city">City</label>' +
+                                   '<input type="text" class="form-control new-city">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-state">State</label>' +
+                                   '<input type="text" class="form-control new-state">' +
+                                 '</div>' +
+                               '</div>');
+  });
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
 
